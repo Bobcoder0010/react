506012a4cd6d8ccaee5a6faaaac4b09cfffcb13c
Bobcoder0010 <dellizulter@gmail.com>
2024-07-04 05:39:23 -0500

    first day 3rd commit

--- firstclass.js
+++ firstclass.js
@@ -5,7 +5,7 @@ const  arr = [1,2,3,4,5,33,45,55]
 
 const arr2 = [4,5,64,2,46,35,24,66]
 
-const newarr= [...arr,arr2];
+const newarr= [...arr,...arr2];
 const {first,seond} = []
 console.log (newarr);
 
@@ -14,7 +14,7 @@ console.log (newarr);
 const obj = {
     name: "bhupal",
     address: "Itahari",
-    phone : "9824345069"
+    phone : "982000000"
 }
 const {name, phone, address} =obj;
 const maya =[name, phone,address]
@@ -22,3 +22,13 @@ const maya =[name, phone,address]
 
 
 console.log(maya);
+const arr5 =[1,4,5,6,35];
+for (let i =0 ;i<arr.length; i++) console.log(arr[i])
+
+
+
+
+
+    const age=9;
+    const isyes = age <10|| age ==12 ? "Yes": "No";
+   console.log(isyes);
