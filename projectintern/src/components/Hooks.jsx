ed75714c3281ff5fd879a7d2de85001563a2ee71
Bobcoder0010 <dellizulter@gmail.com>
2024-07-19 02:21:58 -0500

    change color while clicking button

--- projectintern/src/components/Hooks.jsx
+++ projectintern/src/components/Hooks.jsx
@@ -1,41 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const Hooks = () => {
-  const [value, setValue] = useState(0);
   const [color, setColor] = useState({ backgroundColor: '' });
 
-  useEffect(() => {
-    switch (true) {
-      case value > 4:
-        setValue(0);
-        break;
-      case value > 3:
-        setColor({ backgroundColor: 'white' });
-        break;
-      case value > 2:
-        setColor({ backgroundColor: 'blue' });
-        break;
-      case value > 1:
-        setColor({ backgroundColor: 'black' });
-        break;
-      case value > 0:
-        setColor({ backgroundColor: 'red' });
-        break;
-      default:
-        setColor({ backgroundColor: '' });
-    }
-  }, [value]);
-
-  const incrementValue = () => {
-    setValue(value + 1);
+  const handleColorChange = (newColor) => {
+    setColor({ backgroundColor: newColor });
   };
 
   return (
     <div className="flex flex-wrap">
-      <button onClick={incrementValue} className="p-2 px-4 bg-blue-400">
-        Increment
+      <button onClick={() => handleColorChange('red')} className="p-2 px-4 bg-blue-400">
+        Red
       </button>
-      <span className="bg-black text-white text-3xl">{value}</span>
+      <button onClick={() => handleColorChange('black')} className="p-2 px-4 bg-blue-400">
+        Black
+      </button>
+      <button onClick={() => handleColorChange('blue')} className="p-2 px-4 bg-blue-400">
+        Blue
+      </button>
+      <button onClick={() => handleColorChange('white')} className="p-2 px-4 bg-blue-400">
+        White
+      </button>
       <div style={color} className="w-full h-[20rem] border-2 border-black"></div>
     </div>
   );
