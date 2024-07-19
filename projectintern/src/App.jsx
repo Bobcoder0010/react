796a875e482a11f10a151d1beaaab0cc21eb9360
Bobcoder0010 <dellizulter@gmail.com>
2024-07-19 03:52:14 -0500

    link

--- projectintern/src/App.jsx
+++ projectintern/src/App.jsx
@@ -4,11 +4,17 @@ import Services from './Pages/Services'
 import Contact from './Pages/Contact'
 import About from './Pages/About'
 import NavBar from './components/Navbar'
-import {Routes,Route} from "react-router-dom"
+import {Routes,Route,Link} from "react-router-dom"
 
 const App = () => {
   return (
     <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/services">Services</Link></li>
+      </ul>
      <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
