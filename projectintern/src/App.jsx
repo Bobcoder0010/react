50e2f408c010fb9eced689693c1e5e797cb0a201
Bobcoder0010 <dellizulter@gmail.com>
2024-07-28 03:46:03 -0500

    router

--- projectintern/src/App.jsx
+++ projectintern/src/App.jsx
@@ -4,25 +4,46 @@ import Services from './Pages/Services'
 import Contact from './Pages/Contact'
 import About from './Pages/About'
 import NavBar from './components/Navbar'
-import {Routes,Route,Link} from "react-router-dom"
-
-const App = () => {
+// import {Routes,Route,Link} from "react-router-dom"
+import { createBrowserRouter,RouterProvider,Outlet} from "react-router-dom"
+function App() {
+  const Layout = () => {
+    return (
+      <div>
+        <NavBar />
+        <Outlet />
+      </div>
+    )
+  }
+const Route = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout/>,
+      children: [
+        { path: '/', element: <Home /> },
+        { path: 'about', element: <About /> },
+        { path: 'contact', element: <Contact /> },
+        { path: 'services', element: <Services /> },
+      ]
+    }
+  ]
+)
   return (
     <div>
+      <RouterProvider router={Route}/>
       {/* <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
         <li><Link to="/services">Services</Link></li>
       </ul> */}
-     <Routes>
+     {/* <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/services" element={<Services />} />
-      
-
-      </Routes>    
+      </Routes>     */}
 
 
     </div>
