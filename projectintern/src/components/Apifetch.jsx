b1804231760f5b16cd3d787abe6cde8738ab3af1
Bobcoder0010 <dellizulter@gmail.com>
2024-07-29 04:21:26 -0500

    apifetch

--- projectintern/src/components/Apifetch.jsx
+++ projectintern/src/components/Apifetch.jsx
@@ -1,28 +1,33 @@
 import React, { useEffect } from "react";
+import axios from 'axios';
 
-const ApiFetch = ()=>{
+const ApiFetch = () => {
     const [data, setData] = React.useState([]);
 
-    const handleApi =()=>{
-axios.get('https://jsonplaceholder.typicode.com/todos/1').
-then((res) => {
-    console.log(res.data); 
-    setData(res.data);
-} ).catch(err => console.log(err));
+    useEffect(() => {
+        const handleApi = () => {
+            axios.get('https://jsonplaceholder.typicode.com/posts')
+                .then((res) => {
+                    console.log(res.data);
+                    setData(res.data);
+                })
+                .catch(err => console.log(err));
+        };
 
-} 
- return (
-     <div className="grid grid-cols-4  border-black place-items-center">
-        <div className ="p-8 flex-cols bg-slate-500 text-black">
-            
-            <span className="text-2xl">Id:2</span>
-            <h1 className="text-4xl">Title: </h1>
-            <p className ="text-xl">Body</p>
-             </div>
+        handleApi();
+    }, []);
+
+    return (
+        <div className="grid grid-cols-4 border-black place-items-center">
+            {data.map((value, index) => (
+                <div key={index} className="p-8 flex-cols bg-slate-500 text-black">
+                    <span className="text-2xl">Id: {value.id}</span>
+                    <h1 className="text-4xl">Title: {value.title}</h1>
+                    <p className="text-xl">Body: {value.body}</p>
+                </div>
+            ))}
         </div>
-  
- )
-
+    );
 }
 
 export default ApiFetch;
